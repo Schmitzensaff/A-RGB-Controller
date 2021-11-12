b98dbcb1133d900ba831ad6249cc3c33c0164588
Schmitzensaff <git@github.com>
2021-11-12 21:39:39 +0300

    Alpha v.0.1 Release!

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #define RESIST_BASE 10000   // сопротивление при TEMP_BASE градусах по Цельсию (Ом)
 #define TEMP_BASE 25        // температура, при которой измерено RESIST_BASE (градусов Цельсия)
 #define B_COEF 3435         // бета коэффициент термистора (3000-4000)
-#define STRIP_LED_NUM 50 // Колво светоидиотов в 1 ленте
+#define STRIP_LED_NUM 9 // Колво светоидиотов в 1 ленте
 #define STRIP_LED_NUM2 50 // Колво светоидиотов во 2 ленте
 #define RING_LED_NUM 45 // Колво светоидиотов в 1 кольце
 #define STRIP_LED_TYPE WS2812 // тип светодиодов в ленте
@@ -53,9 +53,9 @@ LiquidCrystal_I2C_Menu lcd(I2C_LCD_ADDRESS,COLS,ROWS);
 
 int x = 0;
 // Яркость
-byte brightness = 0;
-byte brightnessMenu = 0;
-boolean brightnessStatus = 0;
+byte brightness;
+byte brightnessMenu;
+boolean brightnessStatus;
 //---------------------------------
 
 
@@ -72,8 +72,8 @@ boolean RingStatus = 0;
 
 
 // Режимы
-boolean ringMode; 
-boolean stripMode[2];
+byte ringMode = constrain(ringMode, 0, 3); 
+byte stripMode[2] = {constrain(stripMode[0], 0, 2), constrain(stripMode[1], 0, 2)};
 
 // Термистор. Особо не суетить. Работать через getThermTemp(analogRead(THERM))
 float getThermTemp(int resistance){
@@ -87,46 +87,45 @@ float getThermTemp(int resistance){
 }
 //---------------------------------
 
-void strip1st(){} // Прекурсоры
-void strip1md(){}
-void strip1cl(){}
-void strip1sp(){}
-void strip2st(){}
-void strip2md(){}
-void strip2cl(){}
-void strip2sp(){}
-void ring1st(){}
-void ring1md(){}
-void ring1cl(){}
-void ring1sp(){}
-void setBrightness(){}
+
 // Иерархия меню.
-enum {mkBack, mkRoot, mkStrip, mkStrip1, mkStrip2,  mkStripStatus1, mkStripMode1, mkStripColor1, mkStripSpeed1, mkStripStatus2, mkStripMode2, mkStripColor2, mkStripSpeed2,  mkRing, mkRing1, mkRingStatus, mkRingMode, mkRingColor, mkRingSpeed, mkBrightness, };
+enum {mkBack, mkRoot, mkStrip, mkStrip1, mkStrip2,  mkStripStatus1, mkStripMode1, mkStripColor1, mkStripColor1r, mkStripColor1g, mkStripColor1b, mkStripStatus2, mkStripMode2, mkStripColor2, mkStripColor2r,mkStripColor2g,mkStripColor2b, mkRing, mkRing1, mkRingStatus, mkRingMode, mkRingColor, mkRingColorr, mkRingColorg, mkRingColorb, mkBrightness, mkBrightnessStatus, mkBrightnessPercent };
 
 sMenuItem menu[] = {
   {mkBack, mkRoot, "A-RGB"},
     {mkRoot, mkStrip, "Strip"},
       {mkStrip, mkStrip1, "Strip 1"},
-        {mkStrip1, mkStripStatus1, NULL, strip1st},
-        {mkStrip1, mkStripMode1, NULL, strip1md},
-        {mkStrip1, mkStripColor1, NULL, strip1cl},
-        {mkStrip1, mkStripSpeed1, NULL, strip1sp},
+        {mkStrip1, mkStripStatus1, "Status", },
+        {mkStrip1, mkStripMode1, "Mode", },
+        {mkStrip1, mkStripColor1, "Color", },
+          {mkStripColor1, mkStripColor1r, "R", },
+          {mkStripColor1, mkStripColor1g, "G", },
+          {mkStripColor1, mkStripColor1b, "B", }, 
+          {mkStripColor1, mkBack, "Back",},
         {mkStrip1, mkBack, "Back",},
       {mkStrip, mkStrip2, "Strip 2"},
-        {mkStrip2, mkStripStatus2, NULL, strip2st},
-        {mkStrip2, mkStripMode2, NULL, strip2md},
-        {mkStrip2, mkStripColor2, NULL, strip2cl},
-        {mkStrip2, mkStripSpeed2, NULL, strip2sp},
+        {mkStrip2, mkStripStatus2, "Status", },
+        {mkStrip2, mkStripMode2, "Mode", },
+        {mkStrip2, mkStripColor2, "Color",},
+          {mkStrip2, mkStripColor2r, "R", },
+          {mkStrip2, mkStripColor2g, "G", },
+          {mkStrip2, mkStripColor2b, "B", }, 
+          {mkStripColor2, mkBack, "Back",},
         {mkStrip2, mkBack, "Back"},
       {mkStrip, mkBack, "Back"},
     {mkRoot, mkRing, "Ring"},
-      {mkRing, mkRingStatus, NULL, ring1st},
-        {mkRing, mkRingMode, NULL, ring1md},
-        {mkRing, mkRingColor, NULL, ring1cl},
-        {mkRing, mkRingSpeed, NULL, ring1sp},
+      {mkRing, mkRingStatus, "Status", },
+        {mkRing, mkRingMode, "Mode",},
+        {mkRing, mkRingColor, "Color", },
+          {mkRingColor, mkRingColorr, "R", },
+          {mkRingColor, mkRingColorg, "G", },
+          {mkRingColor, mkRingColorb, "B", },
+          {mkRingColor, mkBack, "Back",}, 
         {mkRing, mkBack, "Back"},
-    {mkRoot, mkBrightness, NULL, setBrightness},
-      {mkRing, mkBack, "Back"},
+    {mkRoot, mkBrightness, "Brightness", },
+      {mkBrightness, mkBrightnessStatus, "Status", },
+      {mkBrightness, mkBrightnessPercent, "Level", },
+      {mkBrightness, mkBack, "Back"},
     
 };
 //----------------------------------
@@ -181,29 +180,55 @@ void stripRainbow(byte red, byte green, byte blue, int SpeedDelay){
   int Pixel = random(STRIP_LED_NUM); // Определяем, какой светоидиот загорится
   setPixelStrip(Pixel,red,green,blue); // отправляем  Значения на логмодуль
   FastLED.show();
-  delay(SpeedDelay); // делей
+ // delay(SpeedDelay); // делей
   setPixelStrip(Pixel,0,0,0); // Гасим
 }
 void stripRainbow2(byte red, byte green, byte blue, int SpeedDelay){
   int Pixel = random(STRIP_LED_NUM2); // Определяем, какой светоидиот загорится
-  setPixelStrip(Pixel,red,green,blue); // отправляем  Значения на логмодуль
+  setPixelStrip2(Pixel,red,green,blue); // отправляем  Значения на логмодуль
   FastLED.show();
-  delay(SpeedDelay); // делей
-  setPixelStrip(Pixel,0,0,0); // Гасим
+  //delay(SpeedDelay); // делей
+  setPixelStrip2(Pixel,0,0,0); // Гасим
 }
+
+
+// Радужный поток
+byte counter;
+byte counter2;
+void stripRaindowRiver(){
+  for (int i = 0; i < STRIP_LED_NUM; i++ ) {         // от 0 до первой трети
+    led1[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
+    // умножение i уменьшает шаг радуги
+  }
+  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  FastLED.show();
+ // delay(5); 
+}
+
+void strip2RainbowRiver(){
+  for (int i = 0; i < STRIP_LED_NUM2; i++ ) {         // от 0 до первой трети
+    led2[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
+    // умножение i уменьшает шаг радуги
+  }
+  counter2++;        // counter меняется от 0 до 255 (тип данных byte)
+  FastLED.show();
+ // delay(5);
+}
+
 
 
 // Статической режим
 void stripStatic(){
-  led1[0].setRGB(RGB_R[0], RGB_G[0], RGB_B[0]);  
+  int i = 0; i < ARGB_round; i++;
+  led1[i].setRGB(RGB_R[0], RGB_G[0], RGB_B[0]);  
   FastLED.show();
-    delay(30); // FPS
+   // delay(30); // FPS
     return;
 }
 void stripStatic2(){
   led2[0].setRGB(RGB_R[1], RGB_G[1], RGB_B[1]);  
   FastLED.show();
-    delay(30); // FPS
+   // delay(30); // FPS
     return;
 }
 
@@ -218,7 +243,7 @@ void ringRainbow(byte red, byte green, byte blue, int SpeedDelay){
   int Pixel = random(ARGB_round);
   setPixelStrip(Pixel,red,green,blue);
   FastLED.show();
-  delay(SpeedDelay);
+  //delay(SpeedDelay);
   setPixelStrip(Pixel,0,0,0);
 }
 
@@ -227,11 +252,11 @@ void ringRainbow(byte red, byte green, byte blue, int SpeedDelay){
 void ringStatic(){
   ring1[0].setRGB(RGB_R_R, RGB_G_R, RGB_B_R); // Присваиваем статическое значение
   FastLED.show();
-    delay(30); // FPS
+    //delay(30); // FPS
   return;
 }
 
-/*// Температурный режим
+// Температурный режим
 void ringTemp(){
   byte val = (getThermTemp(analogRead(THERM))); // Получаем температуру в цельсии
   val = constrain(val, CS_TEMP_MIN, CS_TEMP_MAX); // Ограничиваем
@@ -240,9 +265,20 @@ void ringTemp(){
             ring1[led].setRGB(RGB_R_R, RGB_G_R, RGB_B_R); // Цикл
   };
   FastLED.show();
-    delay(30); // FPS
+    // delay(30); // FPS
     return;
-}*/
+}
+
+byte counter3;
+void ringRaindowRiver(){
+  for (int i = 0; i < ARGB_round; i++ ) {         // от 0 до первой трети
+    ring1[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
+    // умножение i уменьшает шаг радуги
+  }
+  counter3++;        // counter меняется от 0 до 255 (тип данных byte)
+  FastLED.show();
+  // delay(5); 
+}
 //------------------------------
 
 
@@ -253,6 +289,7 @@ void strip1(){
     break;
     case 1: stripStatic();
     break;
+    case 2: stripRaindowRiver();
   }
   return;
 }
@@ -262,6 +299,7 @@ void strip2(){
     break;
     case 1: stripStatic();
     break;
+    case 2: strip2RainbowRiver();
   }
   return;
 }
@@ -282,6 +320,9 @@ void ring(){
     break;
     case 1: ringRainbow(random(255), random(255), random(255), 0);
     break;
+    case 2: ringTemp();
+    break;
+    case 3: ringRaindowRiver();
   }
  return;
 }
@@ -290,29 +331,6 @@ void ring(){
 //=================================
 
 
-// Настоящие функции
-void strip1st(){ 
-  StripStatus[0] = lcd.inputVal("Strip 1 status", 0, 1, 0);
-  updateCaption(mkStripStatus1, "Status: (%d)", StripStatus[0]);
-} 
-void strip1md(){}
-void strip1cl(){}
-void strip1sp(){}
-void strip2st(){
-  StripStatus[1] = lcd.inputVal("Strip 2 status", 0, 1, 0);
-  updateCaption(mkStripStatus2, "Status: (%d)", StripStatus[1]);
-}
-void strip2md(){}
-void strip2cl(){}
-void strip2sp(){}
-void ring1st(){
-  RingStatus = lcd.inputVal("Ring status", 0, 1, 0);
-  updateCaption(mkRingStatus, "Status: (%d)", RingStatus); // Тут закончил
-}
-void ring1md(){}
-void ring1cl(){}
-void ring1sp(){}
-void setBrightness(){}
 
 void setup() {
   Serial.begin(9600);
@@ -321,12 +339,13 @@ void setup() {
   lcd.backlight();
   lcd.clear();
   lcd.attachEncoder(ENC_DT, ENC_CLK, ENC_SW);
-  
+
   FastLED.addLeds<STRIP_LED_TYPE, LED_PIN, GRB>(led1, STRIP_LED_NUM).setCorrection( TypicalLEDStrip );; // Инициализация 1 ленты(Вроде) 
   FastLED.addLeds<STRIP_LED_TYPE, LED2_PIN, GRB>(led2, STRIP_LED_NUM2).setCorrection( TypicalLEDStrip );; // Инициализация 2 ленты(Вроде) 
   FastLED.addLeds<RING_LED_TYPE, ARGB_round, GRB>(ring1, RING_LED_NUM).setCorrection( TypicalLEDStrip );; // Инициализация 1 кольца(Вроде)
   lcd.printAt(3, 1, "Schmitzensaff");
   lcd.printAt(3, 2, "Electronics");
+  lcd.printAt(7, 0, "v.0.1 alpha");
   // FastLED.addLeds<LMUSIC_LED_TYPE,ARGB_LMusic, GRB>(led3, LMUSIC_LED_NUM); // Инициализация светомузыки(Вроде)
   delay(1000);
 }
@@ -335,30 +354,86 @@ void setup() {
 void loop() {
    
   map(brightnessMenu, 0, 255, 0, 100);                      // Яркость
-    if (brightnessStatus == true){
-      brightness = map(analogRead(PHOTO), 0, 1023, 0, 255);
-    } else{
+    
       brightness = map(brightnessMenu, 0, 100, 0, 255); 
-    }
+    
   FastLED.setBrightness(brightness); // Уровень яркости
   eEncoderState EncoderState = lcd.getEncoderState();
     uint8_t selectedMenuItem = lcd.showMenu(menu, menuLen, 1);
-   
+
+  strip();
   if (RingStatus == 1) ring();
    switch(selectedMenuItem){
     case mkStripStatus1:{
-      
+      StripStatus[0] = lcd.inputVal<boolean>("Strip 1 status", 0, 1, StripStatus[0]);
+      Serial.print(StripStatus[0]);
     };
     break;
     case mkStripStatus2:{
-      
+      StripStatus[1] = lcd.inputVal<boolean>("Strip 2 status", 0, 1, StripStatus[1]);
     }
     break;
     case mkRingStatus:{
-      
+      RingStatus = lcd.inputVal<boolean>("Ring status", 0, 1, RingStatus);
     }
     break;
-
+    case mkStripMode1:{
+      stripMode[0] = lcd.inputVal<byte>("Strip 1 mode", 0, 2, stripMode[0]);
+    }
+    break;
+    case mkStripMode2:{
+      stripMode[1] = lcd.inputVal<byte>("Strip 2 mode", 0, 2, stripMode[1]);
+    }
+    break;
+    case mkRingMode:{
+      ringMode = lcd.inputVal<byte>("Ring mode", 0, 3, ringMode);
+    }
+    break;
+    case mkStripColor1r:{
+      RGB_R[0] = lcd.inputVal<byte>("Strip 1 red color", 0, 255, RGB_R[0]);
+    }
+    break;
+    case mkStripColor2r:{
+      RGB_R[1] = lcd.inputVal<byte>("Strip 2 red color", 0, 255, RGB_R[1]);
+    }
+    break;
+    case mkRingColorr:{
+      RGB_R_R = lcd.inputVal<byte>("Ring red color", 0, 255, RGB_R_R);
+    }
+    break;
+    case mkStripColor1g:{
+      RGB_G[0] = lcd.inputVal<byte>("Strip 1 green color", 0, 255, RGB_G[0]);
+    }
+    break;
+    case mkStripColor2g:{
+      RGB_G[1] = lcd.inputVal<byte>("Strip 2 green color", 0, 255, RGB_G[1]);
+    }
+    break;
+    case mkRingColorg:{
+      RGB_G_R = lcd.inputVal<byte>("Ring green color", 0, 255, RGB_G_R);
+    }
+    break;
+    case mkStripColor1b:{
+      RGB_B[0] = lcd.inputVal<byte>("Strip 1 blue color", 0, 255, RGB_B[0]);
+    }
+    break;
+    case mkStripColor2b:{
+      RGB_B[1] = lcd.inputVal<byte>("Strip 2 blue color", 0, 255, RGB_B[1]);
+    }
+    break;
+    case mkRingColorb:{
+      RGB_B_R = lcd.inputVal<byte>("Ring blue color", 0, 255, RGB_B_R);
+    }
+    break;
+    case mkBrightnessStatus:{
+      brightnessStatus = lcd.inputVal<boolean>("Brightness mode", 0, 1, brightnessStatus);
+    }
+    break;
+    case mkBrightnessPercent:{
+      brightnessMenu = lcd.inputVal<byte>("Brightness level", 0, 100, brightnessMenu);
+      Serial.print(brightnessMenu);
+    }
+    break;
    }
   
 }
