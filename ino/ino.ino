aa2992e468845db47ab574668aada84c9436c39d
Schmitzensaff <rukki3115@outlook.com>
2022-03-15 17:04:36 +0300

    2.2 Release Вшита поддержка сохранений в EEPROM

--- ino/ino.ino
+++ ino/ino.ino
@@ -1,3 +1,7 @@
+// 2.2 Зашито сохранение в EEPROM
+
+#define INIT_ADDR 511  // номер резервной ячейки
+#define INIT_KEY 250 // ключ резервной ячейки
 #define ST1_NUMLED 9
 #define ST2_NUMLED 45
 
@@ -8,8 +12,10 @@
 #include <FastLED.h>
 #include <Arduino_FreeRTOS.h>
 #include <GParsingStream.h>
+#include <EEPROM.h>
 CRGB st1[ST1_NUMLED];
 CRGB st2[ST2_NUMLED];
+uint32_t SHOWTIMER = 0;
 int dataArray[PARSE_AMOUNT] = {
   0, 0,
   255, 255,
@@ -25,11 +31,24 @@ void parsingTask(void *pvParameters);
 void setup() {
   Serial.begin(9600);
   Serial.println("Connection Succesful");
+  if (EEPROM.read(INIT_ADDR) != INIT_KEY) { // первый запуск
+    Serial.println("This is my first time, Sempai...");
+    for (int i = 0, j = 0; i < PARSE_AMOUNT; i++, j += 2) {
+      EEPROM.put(j, dataArray[i]);
+    }
+    EEPROM.write(INIT_ADDR, INIT_KEY);    // записали ключ
+  }else{
+    for (int i = 0, j = 0; i < PARSE_AMOUNT; i++, j += 2) {
+      EEPROM.get(j, dataArray[i]);
+    }
+  }
+   Serial.println("A-RGB-Controller v.2.2");
   FastLED.addLeds<WS2812, ST1_LEDPIN, GRB>(st1, ST1_NUMLED).setCorrection( TypicalLEDStrip);;
   FastLED.addLeds<WS2812, ST2_LEDPIN, GRB>(st2, ST2_NUMLED).setCorrection( TypicalLEDStrip);;
   xTaskCreate(st1Task, "strip 1 task", 128, NULL, 1, NULL);
   xTaskCreate(st2Task, "strip 2 task", 128, NULL, 1, NULL);
   xTaskCreate(parsingTask, "parsing task", 128, NULL, 1, NULL);
+  Serial.println("Willkommen Herr Schmitz");
   vTaskStartScheduler();
 }
 
@@ -126,6 +145,8 @@ void ST2RS(){
     
 }
 }
+
+
 void ST1RnS(int SpeedDelay){
   for(uint16_t i=0; i<ST1_NUMLED; i++) {
     if (dataArray[2] != 2){ 
@@ -180,13 +201,127 @@ void ST2STC(){
     FastLED.show();
   }
 }
+void ST1CLD(){
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(0, 255, 255);
+     if(dataArray[2] != 3 || i == ST1_NUMLED){
+ 
+
+      break;
+     }
+     
+    }
+     vTaskDelay(20);
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(42, 255, 255);
+      if(dataArray[2] != 3 || i == ST1_NUMLED){
+  
+
+      break;
+     }
+     
+  }
+ vTaskDelay(20);
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(85, 255, 255);
+      if(dataArray[2] != 3 || i == ST1_NUMLED){
+   
+  
+      break;
+     }
+  }
+vTaskDelay(20);
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(128, 255, 255);
+    if(dataArray[2] != 3 || i == ST1_NUMLED){
+      
+      break;
+     }
+  }
+  vTaskDelay(20);
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(171, 255, 255);
+    if(dataArray[2] != 3 || i == ST1_NUMLED){
+
+    
+      break;
+     }
+  } 
+  vTaskDelay(20);
+  for(int i = 0; i < ST1_NUMLED; i++){
+      st1[i] = CHSV(213, 255, 255);
+     if(dataArray[2] != 3 || i == ST1_NUMLED){
+      
+      break;
+     }
+    }
+vTaskDelay(20);
+}
+
+
+void ST2CLD(){
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(0, 255, 255);
+     if(dataArray[3] != 3 || i == ST2_NUMLED){
+ 
+
+      break;
+     }
+     
+    }
+     vTaskDelay(20);
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(42, 255, 255);
+      if(dataArray[3] != 3 || i == ST2_NUMLED){
+  
+
+      break;
+     }
+     
+  }
+ vTaskDelay(20);
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(85, 255, 255);
+      if(dataArray[3] != 3 || i == ST2_NUMLED){
+   
+  
+      break;
+     }
+  }
+vTaskDelay(20);
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(128, 255, 255);
+    if(dataArray[3] != 3 || i == ST2_NUMLED){
+      
+      break;
+     }
+  }
+  vTaskDelay(20);
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(171, 255, 255);
+    if(dataArray[3] != 3 || i == ST2_NUMLED){
+
+    
+      break;
+     }
+  } 
+  vTaskDelay(20);
+ 
+  for(int i = 0; i < ST2_NUMLED; i++){
+      st2[i] = CHSV(213, 255, 255);
+     if(dataArray[3] != 3 || i == ST2_NUMLED){
+      
+      break;
+     }
+    }
+vTaskDelay(20);}
 void st1Task(void *pvParameters){
   while(1){
   
     if(dataArray[0] == 1 && dataArray[2] == 0) ST1RS();
     if(dataArray[0] == 1 && dataArray[2] == 1) ST1STC();
     if(dataArray[0] == 1 && dataArray[2] == 2) ST1RnS(5);  
-   
+    if(dataArray[0] == 1 && dataArray[2] == 3) ST1CLD();
    }
 }
 void st2Task(void *pvParameters){
@@ -194,14 +329,18 @@ void st2Task(void *pvParameters){
     if(dataArray[1] == 1 && dataArray[3] == 0) ST2RS();
     if(dataArray[1] == 1 && dataArray[3] == 1) ST2STC();
     if(dataArray[1] == 1 && dataArray[3] == 2) ST2RnS(5);
-
+    if(dataArray[1] == 1 && dataArray[3] == 3) ST2CLD();
   }
 }
 void parsingTask(void *pvParameters){
   while(1){
     parsingStream((int*)&dataArray);
-  
-    brightnessVoid();
+    if(dataReady()){
+    for(int i = 0, j = 0; i < PARSE_AMOUNT; i++, j += 2){
+      EEPROM.put(j, dataArray[i]);
+    }
+    }
     FastLED.show();
+    brightnessVoid();
   }
 }
